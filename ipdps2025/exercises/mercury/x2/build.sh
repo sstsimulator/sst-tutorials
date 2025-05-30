c08fd8ce282d8a0d3ba8f0f8ce6c160103aa755f
Joseph P. Kenny <jpkenny@sst-devel-new.srn.sandia.gov>
2025-05-29 19:10:53 -0600

    update mercury exercises for container environment

--- ipdps2025/exercises/mercury/x2/build.sh
+++ ipdps2025/exercises/mercury/x2/build.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-/usr/bin/g++ -I/Users/jpkenny/install/sst-elements-hg-fixpartition2-mpi/include/sst/elements/mercury/ -I/Users/jpkenny/install/sst-core-devel-mpi/include -I/Users/jpkenny/install/sst-elements-hg-fixpartition2-mpi/include/sst/elements -I/Users/jpkenny/install/sst-elements-hg-fixpartition2-mpi/include/sst/elements/mask-mpi -fPIC -std=c++17 -c x2.cc
-/usr/bin/g++ x2.o -Wl,-undefined -Wl,dynamic_lookup  -bundle -o libx2.so 
+/usr/bin/g++ -I/home/jammy/sst/install/include -I/home/jammy/sst/install/include/sst/elements -I/home/jammy/sst/install/include/sst/elements/mercury -I/home/jammy/sst/install/include/sst/elements/mask-mpi -fPIC -std=c++17 -c x2.cc
+/usr/bin/g++ x2.o -shared -Wl,-undefined -Wl,dynamic_lookup -o libx2.so 
